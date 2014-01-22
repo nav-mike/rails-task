35282855dd2e7143842abda02a999e19c86a5e86
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2014-01-22 06:47:09 -0500

    Проект создан в rails 4.0

---
+++ turbaza/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turbaza::Application.config.secret_key_base = 'a3628c1a111a0ec788af24a053dedf9c7f56fe7286f031da82e640be612505e85418a6c320adc77a362b355afe1bb67c160ecb56b0210c9899a0fad3638f4c3d'
---
+++ turbaza/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turbaza::Application.config.session_store :cookie_store, key: '_turbaza_session'
