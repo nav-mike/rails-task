3d4c600838cdc790ea475e9463de38f3c64a1598
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-01-31 11:41:46 +0400

    Добавлен запрос на получение количества турбаз в каждом городе.

---
+++ sql/sql request.sql
@@ -0,0 +1,21 @@
+/* Запрос на получение количества турбаз в каждом городе. */
+SELECT
+	cities.name,
+	count (hostels.id) as num_hostels
+FROM
+	cities
+INNER JOIN hostels ON hostels.city_id = cities.id
+GROUP BY
+	cities.id
+	
+	
+/* Запрос на получение количества турбаз в каждом регионе */
+SELECT
+	regions.name,
+	count (hostels.id) as num_hostels
+FROM
+	regions,
+	cities
+INNER JOIN hostels ON hostels.city_id = cities.id AND cities.region_id = regions.id
+GROUP BY
+	regions.id
