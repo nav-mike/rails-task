9a9acc859cd60614ab298e10d786bb19f4cd1b64
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-01-31 12:03:27 +0400

    Добавлен запрос не получение количества турбаз в каждой стране и в каждой географической сущности.

--- sql/sql request.sql
+++ sql/sql request.sql
@@ -22,3 +22,20 @@ GROUP BY
 	
 	
 /* Запрос на получение количества трубаз в каждой стране. */
+SELECT
+	countries.name,
+	count (hostels.id) as num_hostels
+FROM
+	countries,
+	regions,
+	cities
+INNER JOIN hostels ON hostels.city_id = cities.id AND cities.region_id = regions.id AND countries.id = regions.country_id
+GROUP BY
+	countries.id
+
+/* Запрос на получение количества турбаз со всех географических сущностей. */	
+SELECT 	cities.name as name ,count (hostels.id) as num_hostels FROM 	cities INNER JOIN hostels ON hostels.city_id = cities.id GROUP BY 	cities.id
+UNION
+SELECT 	regions.name,count (hostels.id) as num_hostels FROM 	regions, 	cities INNER JOIN hostels ON hostels.city_id = cities.id AND cities.region_id = regions.id GROUP BY 	regions.id
+UNION
+SELECT countries.name, count (hostels.id) as num_hostels FROM countries, regions, cities INNER JOIN hostels ON hostels.city_id = cities.id AND cities.region_id = regions.id AND countries.id = regions.country_id GROUP BY countries.id
